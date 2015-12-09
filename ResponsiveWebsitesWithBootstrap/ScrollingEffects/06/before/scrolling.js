eec41c57fdd83005b9796c61a0bc0de6dd8d16dc
Nora Vasileva <norityyy@gmail.com>
2015-12-09 17:59:32 +0200

    Finish the course

---
+++ ResponsiveWebsitesWithBootstrap/ScrollingEffects/06/before/scrolling.js
@@ -0,0 +1,10 @@
+﻿$(document).ready(function () {
+    $(window).on('load resize', function () {
+        $('.fill-screen').css('height', window.innerHeight);
+    });
+
+    //add Bootstrap's scrollspy
+    $('body').scrollspy({
+        target: '.navbar'
+    });
+});
